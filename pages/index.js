3705107d7967de5afa51e6ecb9e0f16d5881e628
MAdhur-dc <madhur.manocha@designcafe.com>
2023-09-14 04:45:28 +0530

    fix logo path

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import List from "../src/components/People/List";
-import logo from "../../interstellar-search/public/logo.png";
+import logo from "../public/logo.png";
 
 export default function Home({ people }) {
   const [residents, setResidents] = useState([]);
